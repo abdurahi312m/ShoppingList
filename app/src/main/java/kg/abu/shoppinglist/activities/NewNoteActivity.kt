c7d4961f80f13fd01ad927f9915181dea2f2cb5d
Abdurah1m <talantbekuuluabdurahim3@gmail.com>
2023-08-22 14:31:49 +0600

    edited notes

--- app/src/main/java/kg/abu/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/kg/abu/shoppinglist/activities/NewNoteActivity.kt
@@ -16,12 +16,14 @@ import java.util.Locale
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewNoteBinding
+    private var note: NoteItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
         actionBarSettings()
+        getNote()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -29,9 +31,21 @@ class NewNoteActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    private fun getNote() {
+        val sNote = intent.getSerializableExtra(NoteFragment.NEW_NOTE_KEY)
+        if (sNote != null) {
+            note = sNote as NoteItem
+            fillNote()
+        }
+    }
+
+    private fun fillNote() = with(binding) {
+        edTitle.setText(note?.title)
+        edDescription.setText(note?.content)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.id_save) {
-            Toast.makeText(applicationContext, "Saved", Toast.LENGTH_SHORT).show()
             setMainResult()
         } else if (item.itemId == android.R.id.home) {
             finish()
@@ -40,11 +54,28 @@ class NewNoteActivity : AppCompatActivity() {
     }
 
     private fun setMainResult() {
+        var editState = "new"
+        val tempNote: NoteItem? = if (note == null) {
+            Toast.makeText(applicationContext, "Saved", Toast.LENGTH_SHORT).show()
+            createNewNote()
+        } else {
+            editState = "update"
+            Toast.makeText(applicationContext, "Updated", Toast.LENGTH_SHORT).show()
+            updateNote()
+        }
         val i = Intent().apply {
-            putExtra(NoteFragment.NEW_NOTE_KEY, createNewNote())
+            putExtra(NoteFragment.NEW_NOTE_KEY, tempNote)
+            putExtra(NoteFragment.EDIT_STATE_KEY, editState)
         }
         setResult(RESULT_OK, i)
         finish()
+    }
+
+    private fun updateNote(): NoteItem? = with(binding) {
+        return note?.copy(
+            title = edTitle.text.toString(),
+            content = edDescription.text.toString()
+        )
     }
 
     private fun createNewNote(): NoteItem {
@@ -54,7 +85,7 @@ class NewNoteActivity : AppCompatActivity() {
             binding.edDescription.text.toString(),
             getCurrentTime(),
             ""
-            )
+        )
     }
 
     private fun getCurrentTime(): String {
