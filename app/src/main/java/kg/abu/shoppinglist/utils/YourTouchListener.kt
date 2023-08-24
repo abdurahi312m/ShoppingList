8340ae01549b13cc01900b30acb20ef9dcf03f50
Abdurah1m <talantbekuuluabdurahim3@gmail.com>
2023-08-24 11:58:03 +0600

    created markup for ColorPicker, added drag and drop for ColorPicker, added ActionMenu and AdjustResize for EditText(edDescription)

---
+++ app/src/main/java/kg/abu/shoppinglist/utils/YourTouchListener.kt
@@ -0,0 +1,27 @@
+package kg.abu.shoppinglist.utils
+
+import android.annotation.SuppressLint
+import android.view.MotionEvent
+import android.view.View
+import android.view.View.OnTouchListener
+
+class YourTouchListener : OnTouchListener {
+    private var xDelta = 0.0f
+    private var yDelta = 0.0f
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                xDelta = v?.x!! - event.rawX
+                yDelta = v.y - event.rawY
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                v?.x = xDelta + event.rawX
+                v?.y = yDelta + event.rawY
+            }
+        }
+        return true
+    }
+}
